f2653e08358a3d2f37975d5f7d7b2acb6c4400a3
satsukies <satsukies@gmail.com>
2015-11-14 15:47:52 +0900

    Initial Commit

---
+++ library/src/main/java/net/ddns/satsukies/togglecolors/ToggleColors.java
@@ -0,0 +1,27 @@
+package net.ddns.satsukies.togglecolors;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ToggleButton;
+
+/**
+ * Created by satsukies on 15/11/14.
+ */
+public class ToggleColors extends ToggleButton {
+//    public ToggleColors(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+//        super(context, attrs, defStyleAttr, defStyleRes);
+//    }
+
+    public ToggleColors(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ToggleColors(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ToggleColors(Context context) {
+        super(context);
+    }
+}
